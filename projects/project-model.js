63db87253e2c1b48366b460ba751bcac9544dfcc
MadisonViaLux <madison.s.mckown@gmail.com>
2019-10-11 11:46:22 -0700

    working out bugs on getting tasks

--- projects/project-model.js
+++ projects/project-model.js
@@ -4,6 +4,9 @@ const db = require('../data/db-config')
 module.exports = {
     find,
     findById,
+    // findRes,
+    // findResById,
+    findTask
 }
 
 function find(){
@@ -13,4 +16,20 @@ function find(){
 function findById(id){
     return db('projects')
         .where({id})
+}
+
+// function findRes(){
+//     return db('resource')
+// }
+
+// function findResById(id){
+//     return db('resource')
+//         .where({id})
+// }
+
+function findTask(id){
+    return db('projects AS p')
+        .join('task AS t', {'p.id': 't.project_id'})
+        .select('description', 'notes', 'completed')
+        .where({project_id: id})
 }
--- projects/project-router.js
+++ projects/project-router.js
@@ -23,4 +23,41 @@ router.get('/:id', (req, res) => {
 
 
 
+
+// router.get('/res', (req, res) => {
+//     Pro.findRes()
+//         .then(pro => res.json(pro))
+//         .catch(err => {
+//             res.status(500).json({ message: 'failed to get projects table' })
+//         })
+// })
+
+// router.get('/res/:id', (req, res) => {
+//     const { id } = req.params
+
+//     Pro.findResById(id)
+//         .then(pro => res.json(pro))
+//         .catch(err => {
+//             res.status(500).json({ message: 'failed to get projects by ID' })
+//         })
+// })
+
+
+
+
+
+router.get('/:id/task', (req, res) => {
+    const { id } = req.params
+
+    Pro.findTask(id)
+        .then(pro => res.json(pro))
+        .catch(err => {
+            res.status(500).json({ message: 'failed to get tasks table' })
+        })
+})
+
+
+
+
+
 module.exports = router
